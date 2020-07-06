39a281daa3d64c98303ff34b83333e24328797fc
Charles Poulsen <chpouls@microsoft.com>
2020-07-06 12:20:26 +0800

    Added docker

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-var express = require('express');
-var app = express();
+'use strict';
 
-app.get('/', function(req, res){
-   res.send("Hello world!");
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
 
-app.listen(3000);
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
